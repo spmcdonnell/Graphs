f2864be919a3452c2538ba19d46f2f9575cf3db7
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2020-02-21 21:48:45 -0500

    Add interview questions

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -143,3 +143,45 @@ ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
              (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
 
 print(earliest_ancestor(ancestors, 6))
+
+
+# General Info
+# ------------
+
+# 1. "Highlight previous experience living abroad and being able to adapt. Highlight language learning and connection to learning new skills/programming languages, and that you have learned how to learn"
+
+# 2. "Highlight that because your current company is small, you are required to do a variety of things - never say 'Hey, its not my job"
+
+# 3. "Highlight that you have taught English which has sharpened communication with people."
+
+# 4. "Ask questions effectively so as to get clear answers and not offload work to someone else"
+
+# 5. "Backflip onto mats analogy"
+
+
+# Scenarios
+# ---------
+
+# Problems/Conflicts
+
+# 1. "Designers don't know how to design for the web, rather for catelogs - I made resources for then"
+
+# 2. "Disagreement with other devs. 2 strategies. 1. Let them go through and see the problem/solution for themselves. 2. Offer to try a solution just to see if it works, at the very least it eliminates a possibility"
+
+# Initiative/Proud of
+
+# 3. "Company was using shortcodes on pages and clients were breaking the page. I proposed using custom fields and figured out how to do it. It is now the standard we use for development"
+
+# 4. "We made generic sites with one specific wordpress template. Boss didn't want to deviate and wasn't sure anything else was possbile. Designers wanted something new and creative. I figured out how to make new styles of sites, and now our range of products is greater"
+
+# Difficult/Stressful situations
+
+# 5. "Student wasn't interested in learning English. I related my own successfully learning techniques and I found out what kinds of things they were interested in and found English language versions so they could have fun while learning"
+
+# 6. "Designer was late getting us a design, and my Boss had a meeting the next day with a client. I stayed up late and got it done in time, keeping in mind that having to do this is a rare occurance"
+
+# Strengths/Weaknesses
+
+# 7. "Strength is extreme attention to detail, which is a huge asset to dev companies and can be a hard thing to teach"
+
+# 8. "Weakness is design. I studied basic design concepts, bought a tablet, learned how to use photoshop. Having some level of skill is much better than having none. I also am upfront about my weaknesses so they can be known and accounted for by the team."
